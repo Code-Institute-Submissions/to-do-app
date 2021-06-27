26c3aaea1ec4daba8976a14365755a6b96d13920
Jacob <Jacobshortall@gmail.com>
2021-06-27 17:22:30 +0100

    Add code to close modal using cross icon & add cursor pointer to modal button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,15 @@
 const addNoteButton = document.getElementById("add-note");
 addNoteButton.addEventListener("click", showModal);
 
-/**
- * displays modal for user to enter text
- */
+/** displays modal for user to enter text */
 function showModal() {
     document.getElementById("modal").style.display = "flex";
+
+    // add event listener to modal cross
+    document.getElementsByClassName("fa-times")[0].addEventListener("click", closeModal);
+}
+
+/** closes modal */
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
 }
