d6a811c6f9873a0548200db3a22e97063c586d31
Jacob <Jacobshortall@gmail.com>
2021-06-28 14:00:42 +0100

    Add code to add user input as a note to to-do section

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,10 @@ function showModal() {
     // add event listener to modal cross
     const modalCross = document.getElementsByClassName("fa-times")[0];
     modalCross.addEventListener("click", closeModal);
+
+    // add event listener to "add note" button
+    const confNote = document.getElementById("add-conf");
+    confNote.addEventListener("click", addNote);
 }
 
 /** closes modal */
@@ -27,4 +31,29 @@ function closeModal() {
     // removes darkened overlay
     const overlay = document.getElementById("overlay");
     overlay.style.display = "none";
+}
+
+/** adds note to to do list */
+function addNote() {
+    const input = document.getElementById("note-input").value;
+    const toDoContainer = document.getElementById("to-do");
+
+    // check text area isn't empty
+    if (input) {
+        // creating element to be added to list
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <p>${input}</p>
+            <div class="button-cont">
+                <button class="complete">complete</button>
+                <button class="remove">remove</button>
+            </div>
+        `;
+        div.className = "to-do-item";
+
+        // add created element
+        toDoContainer.appendChild(div);
+    } else {
+        alert("Please enter some text.");
+    }
 }
